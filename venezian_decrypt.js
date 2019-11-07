3f8593f9df0488e9e3bad68ebfa6f0190e939749
Luca Naterop <luca@dhcp-wlan-uzh-10-12-205-173.uzh.ch>
2019-11-07 13:57:44 +0100

    decrypt venezian

--- venezian_decrypt.js
+++ venezian_decrypt.js
@@ -11,15 +11,19 @@ const asciNumber = letter.charCodeAt(0);
 const l = String.fromCharCode(asciNumber);
 const message = "Hallo, ich bins der Lucano. Ich hoffe du kannst diese Nachricht knacken.";
 const encryptedMessage = translateMessage(message, 5);
-console.log(encryptedMessage)
-
-
-
-
+decryptVenezian(encryptedMessage);
 
 
 // function definitions start here
 
+function decryptVenezian(message) {
+	console.log('---------- DECRYPT MESSAGE -------------');
+	console.log(message);
+	for(let n = -50; n<=50; n++) {
+		console.log ('n =',n);
+		console.log(translateMessage(message,n));
+	}
+}
 
 function getVenezianEncryptedMessage(message, n) {
 	// returns encrypted message
@@ -32,7 +36,7 @@ function translateMessage(message, n) {
 	let translatedMessage = '';
 
 	for(let i=0; i < message.length; i++) {
-		const asciNumber = message[i].charCodeAt(0);
+		const asciNumber = message[i].charCodeAt(0)%256;
 		const newNumber = asciNumber + n;
 		const newLetter = String.fromCharCode(newNumber);
 		translatedMessage += newLetter;
